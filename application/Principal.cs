c1ffedd9965819d8abe21433a538165dd64503d6
Heberth Santos <86444094+Alyah12@users.noreply.github.com>
2024-02-22 09:21:07 -0300

    Upload do projeto

---
+++ application/Principal.cs
@@ -0,0 +1,31 @@
+﻿namespace TesteNovo.application;
+
+public class Principal
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Digite o nome do dono da conta ");
+        String name = Console.ReadLine()!;
+        Console.Write("Digite sua idade: ");
+        int idade = int.Parse(Console.ReadLine()!);
+        Console.Write("Digite seu CPF: ");
+        String cpf = Console.ReadLine()!;
+        Console.Write("Qual o seu sálario ?");
+        int balance = int.Parse(Console.ReadLine()!);
+        Console.Write("Você é uma pessoa júridica? (S/M) ");
+        char juridica = char.Parse(Console.ReadLine()!);
+
+        Boolean pessoaJuridica = true;
+        if (juridica == 'S')
+        {
+            pessoaJuridica = true;
+        }
+        else
+        {
+            pessoaJuridica = false;
+        }
+
+        Conta cc = new Conta(name, idade, cpf, balance, pessoaJuridica);
+
+    }
+}
---
+++ entities/Conta.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+
+namespace TesteNovo;
+
+public class Conta : IEnumerable
+{
+    private String name;  
+    private int idade;
+    private String cpf;
+    private int balance;
+    private Boolean pessoaJuridica;
+
+    public string Name
+    {
+        get => name;
+        set => name = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    public int Idade
+    {
+        get => idade;
+        set => idade = value;
+    }
+
+    public string Cpf
+    {
+        get => cpf;
+        set => cpf = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    public int Balance
+    {
+        get => balance;
+        set => balance = value;
+    }
+
+    public bool PessoaJuridica
+    {
+        get => pessoaJuridica;
+        set => pessoaJuridica = value;
+    }
+
+    public Conta()
+    {
+        
+    }
+
+    public Conta(String name, int idade, string cpf, bool pessoaJuridica)
+    {
+        this.name = name;
+        this.idade = idade;
+        this.cpf = cpf;
+        this.pessoaJuridica = pessoaJuridica;
+    }
+
+    public Conta(string name, int idade, string cpf, int balance, bool pessoaJuridica)
+    {
+        this.name = name;
+        this.idade = idade;
+        this.cpf = cpf;
+        this.balance = balance;
+        this.pessoaJuridica = pessoaJuridica;
+    }
+
+    public int jurosSobConta()
+    {
+        return balance - 5 * 100;
+    }
+
+    public int Sacar(int desconto)
+    {
+        return jurosSobConta() - desconto;
+    }
+
+    public IEnumerator GetEnumerator()
+    {
+        throw new NotImplementedException();
+    }
+}
