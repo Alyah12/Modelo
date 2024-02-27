625abc89c1a5e7a16f2905cf8e2c5b2154faf2b4
Roberto Pereira <robpmarques@gmail.com>
2024-02-26 21:44:39 -0300

    rollback project folder changes

--- obj/Debug/net8.0/TesteNovo.AssemblyInfo.cs
+++ obj/Debug/net8.0/TesteNovo.AssemblyInfo.cs
@@ -6,8 +6,10 @@
 //     the code is regenerated.
 // </auto-generated>
 //------------------------------------------------------------------------------
+
 using System;
 using System.Reflection;
+
 [assembly: System.Reflection.AssemblyCompanyAttribute("TesteNovo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
